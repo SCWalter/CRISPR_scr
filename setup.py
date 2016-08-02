cc2656f94253a30dbb4619deecdfb6abab892c83
SCWalter <yunhail@stanford.edu>
2016-08-01 23:44:49 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+setup(
+    name = "crisprscr",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = ['say_hello.py'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['docutils>=0.3'],
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+        'hello': ['*.msg'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Yunhai Luo",
+    author_email = "me@example.com",
+    description = "This is a pipeline for analyzing CRISPR screen related data",
+    license = "MIT",
+    keywords = "CRISPR screen pipeline",
+    url = "https://github.com/SCWalter/CRISPR_scr",   # project home page
+
+    # could also include long_description, download_url, classifiers, etc.
+)
+)
