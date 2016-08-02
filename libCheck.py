513a6c962fb5c79bc1bef8a3d950c74783c4c194
SCWalter <yunhail@stanford.edu>
2016-08-02 00:47:54 -0700

    Create libCheck.py

---
+++ libCheck.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Check the quality (guides representation) of CRISPR library')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print args.accumulate(args.integers)
+
+### Keep base 1 to 50
+### Keep 100% QS>30
+### Remove duplicates
+### Keep only 20bp guides
+fastx_trimmer -Q33 \
+              -l 50 \
+              -i /scratch/users/yunhail/CRISPRscreen07292016/sample1_S1_L001_R1_001.fastq \
+    | fastx_collapser -Q33 -v > /scratch/users/yunhail/CRISPRscreen07292016/S1_trimmed_filtered_dedup.fasta
+perl /home/yunhail/FAST-iCLIP/bin/fasta_to_fastq.pl /scratch/users/yunhail/CRISPRscreen07292016/S1_trimmed_filtered_dedup.fasta \
+    | fastx_trimmer -Q33 \
+                    -f 31 > /scratch/users/yunhail/CRISPRscreen07292016/S1_processed_guides.fastq
+fastx_trimmer -Q33 \
+              -l 50 \
+              -i /scratch/users/yunhail/CRISPRscreen07292016/sample2_S2_L001_R1_001.fastq \
+    | fastx_collapser -Q33 -v > /scratch/users/yunhail/CRISPRscreen07292016/S2_trimmed_filtered_dedup.fasta
+perl /home/yunhail/FAST-iCLIP/bin/fasta_to_fastq.pl /scratch/users/yunhail/CRISPRscreen07292016/S2_trimmed_filtered_dedup.fasta \
+    | fastx_trimmer -Q33 \
+                    -f 31 > /scratch/users/yunhail/CRISPRscreen07292016/S2_processed_guides.fastq
+
+### Align to expected guides
+bowtie2 -p 8 \
+        -L 20 \
+        -N 0 \
+        --no-1mm-upfront \
+        -x /scratch/users/yunhail/CRISPRscreen07292016/bowtie2_index/PMJ08012016 \
+        S1_processed_guides.fastq \
+        --un S1_processed_guides_unmapped.fastq \
+        -S S1_processed_guides_mapped.sam > S1_processed_guides_bowtie2_stats.txt 2>&1
+bowtie2 -p 8 \
+        -L 20 \
+        -N 0 \
+        --no-1mm-upfront \
+        -x /scratch/users/yunhail/CRISPRscreen07292016/bowtie2_index/PMJ08012016 \
+        S2_processed_guides.fastq \
+        --un S2_processed_guides_unmapped.fastq \
+        -S S2_processed_guides_mapped.sam > S2_processed_guides_bowtie2_stats.txt 2>&1
+
+### Count mapped reads from a SAM file
+samtools view -F 260 S1_processed_guides_mapped.sam \
+    | cut -f 3 \
+    | sort \
+    | uniq -c \
+    | awk '{printf("%s\t%s\n", $2, $1)}' > S1_counts.txt
+samtools view -F 260 S2_processed_guides_mapped.sam \
+    | cut -f 3 \
+    | sort \
+    | uniq -c \
+    | awk '{printf("%s\t%s\n", $2, $1)}' > S2_counts.txt
