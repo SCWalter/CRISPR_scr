338f8d27ea92848a9c2c5f3a3f4421df656a56e8
SCWalter <yunhail@stanford.edu>
2016-08-03 00:36:30 -0700

    script draft

--- libCheck.py
+++ libCheck.py
@@ -1,62 +1,59 @@
 #!/usr/bin/env python
 
-import argparse
+import argparse, os
 
 parser = argparse.ArgumentParser(description='Check the quality (guides representation) of CRISPR library')
-parser.add_argument('-i', '--input', help='illumina fastq file(s)', required=True)
+parser.add_argument('-i', '--input', help='illumina fastq file(s)', nargs='+', required=True)
 bt2index = parser.add_mutually_exclusive_group(required=True)
 bt2index.add_argument('-a', '--fasta', help='sequences of expected guides. Will be used for making bowtie2 index')
 bt2index.add_argument('--bt2', help='bowtie2 index for expected guides.')
-parser.add_argument("--1mm", help="allow 1 mismatch during bowtie2 alignment. Default does not all mismatch.", action="store_true")
+parser.add_argument("--mm", help="allow 1 mismatch during bowtie2 alignment. Default does not all mismatch.", action="store_true")
 parser.add_argument('-o', '--outpath', help='path to put outputs', required=True)
 
 args = parser.parse_args()
 
-### Keep base 1 to 50
-### Keep 100% QS>30
-### Remove duplicates
-### Keep only 20bp guides
-fastx_trimmer -Q33 \
-              -l 50 \
-              -i /scratch/users/yunhail/CRISPRscreen07292016/sample1_S1_L001_R1_001.fastq \
-    | fastx_collapser -Q33 -v > /scratch/users/yunhail/CRISPRscreen07292016/S1_trimmed_filtered_dedup.fasta
-perl /home/yunhail/FAST-iCLIP/bin/fasta_to_fastq.pl /scratch/users/yunhail/CRISPRscreen07292016/S1_trimmed_filtered_dedup.fasta \
-    | fastx_trimmer -Q33 \
-                    -f 31 > /scratch/users/yunhail/CRISPRscreen07292016/S1_processed_guides.fastq
-fastx_trimmer -Q33 \
-              -l 50 \
-              -i /scratch/users/yunhail/CRISPRscreen07292016/sample2_S2_L001_R1_001.fastq \
-    | fastx_collapser -Q33 -v > /scratch/users/yunhail/CRISPRscreen07292016/S2_trimmed_filtered_dedup.fasta
-perl /home/yunhail/FAST-iCLIP/bin/fasta_to_fastq.pl /scratch/users/yunhail/CRISPRscreen07292016/S2_trimmed_filtered_dedup.fasta \
-    | fastx_trimmer -Q33 \
-                    -f 31 > /scratch/users/yunhail/CRISPRscreen07292016/S2_processed_guides.fastq
+if args.bt2:
+	bt2index = os.path.basename(args.a).replace(".fasta", "")
+	cmd = "bowtie2-build {} {}".format(args.a, bt2index)
+	print(cmd)
+	os.system(cmd)
+else: bt2index = args.bt2
+	
+for fastq in args.input:
+	name = os.path.basename(fastq)
+	trimdedup = name.replace(".fastq", "_trimmed_dedup.fasta")
+	guideseq = name.replace(".fastq", "_processed_guides.fastq")
+	bt2unmap = name.replace(".fastq", "_unmapped.fastq")
+	bt2map = name.replace(".fastq", "_mapped.sam")
+	bt2stats = name.replace(".fastq", "_stats.txt")
+	gcounts = name.replace(".fastq", "_counts.txt")
 
-### Align to expected guides
-bowtie2 -p 8 \
-        -L 20 \
-        -N 0 \
-        --no-1mm-upfront \
-        -x /scratch/users/yunhail/CRISPRscreen07292016/bowtie2_index/PMJ08012016 \
-        S1_processed_guides.fastq \
-        --un S1_processed_guides_unmapped.fastq \
-        -S S1_processed_guides_mapped.sam > S1_processed_guides_bowtie2_stats.txt 2>&1
-bowtie2 -p 8 \
-        -L 20 \
-        -N 0 \
-        --no-1mm-upfront \
-        -x /scratch/users/yunhail/CRISPRscreen07292016/bowtie2_index/PMJ08012016 \
-        S2_processed_guides.fastq \
-        --un S2_processed_guides_unmapped.fastq \
-        -S S2_processed_guides_mapped.sam > S2_processed_guides_bowtie2_stats.txt 2>&1
-
-### Count mapped reads from a SAM file
-samtools view -F 260 S1_processed_guides_mapped.sam \
-    | cut -f 3 \
-    | sort \
-    | uniq -c \
-    | awk '{printf("%s\t%s\n", $2, $1)}' > S1_counts.txt
-samtools view -F 260 S2_processed_guides_mapped.sam \
-    | cut -f 3 \
-    | sort \
-    | uniq -c \
-    | awk '{printf("%s\t%s\n", $2, $1)}' > S2_counts.txt
+	### Keep base 1 to 50
+	### Keep 100% QS>30
+	### Remove duplicates
+	cmd_trim = "fastx_trimmer -Q33 -l 50 -i {}".format(fastq)
+	cmd_dedup = "fastx_collapser -Q33 -v > {}".format(args.outpath + trimdedup)
+	cmd = ' | '.join(cmd_trim, cmd_dedup)
+	print(cmd)
+	os.system(cmd)
+	
+	### Make arbitrary fastq since collapser gives only fasta
+	### Keep only 20bp guides
+	cmd_fastq = "perl fasta_to_fastq.pl {}".format(args.outpath + trimdedup)
+	cmd_trim = "fastx_trimmer -Q33 -f 31 > {}".format(args.outpath + guideseq)
+	cmd = ' | '.join(cmd_fastq, cmd_trim)
+	print(cmd)
+	os.system(cmd)
+	
+	### Align to expected guides
+	if args.mm:
+		cmd_bt2 = "bowtie2 -p 8 -L 20 -N 0 -x {} {} --un {} -S {} > {} 2>&1".format(bt2index, args.outpath + guideseq, bt2unmap, bt2map, bt2stats)
+	else:
+		cmd_bt2 = "bowtie2 -p 8 -L 20 -N 0 --no-1mm-upfront -x {} {} --un {} -S {} > {} 2>&1".format(bt2index, args.outpath + guideseq, bt2unmap, bt2map, bt2stats)
+	print(cmd)
+	os.system(cmd)
+	
+	### Count mapped reads from a SAM file
+	cmd = "samtools view -F 260 {} | cut -f 3 | sort | uniq -c | awk '{printf(\"%s\t%s\n\", $2, $1)}' > {}".format(bt2map, gcounts)
+	print(cmd)
+	os.system(cmd)
