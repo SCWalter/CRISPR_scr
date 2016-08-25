d9fa37edd6fc5cc423580ea15850143cc2e4813a
SCWalter <yunhail@stanford.edu>
2016-08-24 22:46:01 -0700

    Fix test.sh for file input and out path

--- test/test.sh
+++ test/test.sh
@@ -3,9 +3,9 @@
 source activate crispr_scr
 
 python ../guideCount.py -i test1.fastq test2.fastq \
-       -t /users/test/CRISPR/Table_Guides.txt \
+       -t ./Table_Guides.txt \
        -r 1:9 \
        -g 31:50 \
-       -o /users/test/CRISPR/
+       -o ./
 
 source deactivate
