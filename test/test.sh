8a8761e912c4bfb480c0eff3eb2e093c602ba07f
SCWalter <yunhail@stanford.edu>
2016-08-24 22:41:40 -0700

    Change name to reflect potential usage in counting guides for all purpose.

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 source activate crispr_scr
 
-python libCheck.py -i test1.fastq test2.fastq \
+python ../guideCount.py -i test1.fastq test2.fastq \
        -t /users/test/CRISPR/Table_Guides.txt \
        -r 1:9 \
        -g 31:50 \
