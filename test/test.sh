b994732e60f7d8f72857f4f3185c8e8763e880df
SCWalter <yunhail@stanford.edu>
2016-08-24 23:12:10 -0700

    Get the full directory name of the script no matter where it is being called from.

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Get the full directory name of the script no matter where it is being called from.
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in/246128#246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 source activate crispr_scr
 
-python ../guideCount.py -i test1.fastq test2.fastq \
-       -t ./Table_Guides.txt \
+python $DIR/../guideCount.py -i test1.fastq test2.fastq \
+       -t $DIR/Table_Guides.txt \
        -r 1:9 \
        -g 31:50 \
-       -o ./
+       -o $DIR/
 
 source deactivate
